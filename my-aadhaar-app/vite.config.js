3426bd68120aa5e1fdab642f7faf1f0e0e122862
IHSIRVP <rishi.vijaywargiya@plaksha.edu.in>
2025-06-09 12:34:56 +0530

    putting url to backend and frontend

--- my-aadhaar-app/vite.config.js
+++ my-aadhaar-app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: true, // 👈 allow external access (e.g., from phone)
     port: 5174,
     allowedHosts: [
-      'dea9-2401-4900-57f8-6449-29d1-3329-240f-a2d6.ngrok-free.app' // ✅ no protocol (no https://)
+      '36a8-2401-4900-56d3-624e-500d-6105-78f5-e8c8.ngrok-free.app' // ✅ no protocol (no https://)
     ],
   },
   plugins: [react(), tailwindcss()],
